3a5ec2b668113e94cb535b7689950cdf891e9a58
Chris Coffey <cmcof273@gmail.com>
2016-11-06 22:30:13 -0500

    Switched over to vectors for quicksort analysis

--- algorithms/app/Main.hs
+++ algorithms/app/Main.hs
@@ -2,9 +2,13 @@ module Main where
 
 import QuicksortCounting
 
+import qualified Data.Vector as V
+
 main :: IO ()
 main = do
     lines <- lines <$> readFile "data/QuicksortCounting.txt"
     let ns = read <$> lines :: [Int]
-    print $ quicksort' ns
+    print . snd $ quicksort2 (V.fromList ns)
+    print . snd $ quicksort2' (V.fromList ns)
+    print . snd $ quicksort2'' (V.fromList ns)
 
--- algorithms/src/QuicksortCounting.hs
+++ algorithms/src/QuicksortCounting.hs
@@ -1,7 +1,74 @@
 module QuicksortCounting (
     quicksort,
-    quicksort'
+    quicksort',
+    quicksort2,
+    quicksort2',
+    quicksort2''
 ) where
+
+import Data.List (sortBy)
+import qualified Data.Vector as V
+
+--Uses the first element as the pivot
+quicksort2 :: Ord a => V.Vector a -> (V.Vector a, Int)
+quicksort2 v
+    | V.null v          = (v, 0)
+    | V.length v == 1   = (v, 0) 
+    | otherwise         = let
+        pivot   = V.head v
+        (l, r)  = partition' pivot v
+        (lt, a) = quicksort2 l
+        (gt, b) = quicksort2 r
+        comps   = length v - 1
+        in (lt V.++ V.singleton pivot V.++ gt, a + b + comps)
+
+--Uses the last element as the pivot
+quicksort2' :: Ord a => V.Vector a -> (V.Vector a, Int)
+quicksort2' v 
+    | V.null v          = (v, 0)
+    | V.length v == 1   = (v, 0) 
+    | otherwise         = let
+        pivot   = V.last v
+        v'      = v `V.update` (V.fromList [(0, pivot), (V.length v - 1, V.head v)]) --constant time swap to preserve partition' implementation
+        (l, r)  = partition' pivot v'
+        (lt, a) = quicksort2' l
+        (gt, b) = quicksort2' r
+        comps   = length v - 1
+        in (lt V.++ V.singleton pivot V.++ gt, a + b + comps)
+
+--
+quicksort2'' :: Ord a => V.Vector a -> (V.Vector a, Int)
+quicksort2'' v 
+    | V.null v          = (v, 0)
+    | V.length v == 1   = (v, 0) 
+    | otherwise         = let
+        (pivot, idx)   = findPivot
+        v'      = v `V.update` (V.fromList [(0, pivot), (idx, V.head v)]) --constant time swap to preserve partition' implementation
+        (l, r)  = partition' pivot v'
+        (lt, a) = quicksort2'' l
+        (gt, b) = quicksort2'' r
+        comps   = length v - 1
+        in (lt V.++ V.singleton pivot V.++ gt, a + b + comps)
+    where 
+    middleIndex = if even $ V.length v then V.length v `div` 2 else (V.length v `div` 2 ) + 1
+    findPivot = head . tail $ sortBy (\a b-> fst a `compare` fst b) [(V.head v, 0), 
+                                    (V.last v, V.length v - 1),
+                                    (v V.! middleIndex, middleIndex)]
+
+partition' :: Ord a => a -> V.Vector a -> (V.Vector a, V.Vector a)
+partition' pivot v = let
+    (v', lowEdge , _) = foldl swap (v, 0, 0) v
+    v'' = v' `V.update` (V.fromList [(lowEdge, V.head v'), (0, v' V.! lowEdge)])
+    in (V.take lowEdge v'', V.drop (lowEdge + 1) v'')
+    where 
+    swap (vec, lowEdge, focus) _
+        | vec V.! focus < pivot = let
+            nextLowest = lowEdge + 1
+            e  = vec V.! focus
+            vec' = vec `V.update` (V.fromList [(focus, vec V.! nextLowest), 
+                                               (nextLowest, e)])
+            in (vec', nextLowest, focus + 1)
+        | otherwise     = (vec, lowEdge, focus + 1)
 
 quicksort :: Ord a => [a] -> ([a], Int)
 quicksort [] = ([], 0)
