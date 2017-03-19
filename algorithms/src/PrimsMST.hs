481529e8769a51dba6ed49184aa56f6d994a41ef
Chris Coffey <cmcof273@gmail.com>
2017-03-19 15:16:56 -0400

    Prims MST algorithm

--- algorithms/src/PrimsMST.hs
+++ algorithms/src/PrimsMST.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiWayIf #-}
+
 module PrimsMST (
     mstCost   
 ) where
@@ -10,15 +12,67 @@ import Text.Parsec
 import Text.Parsec.Char
 import Text.Parsec.String
 
-newtype Vertex = Vertx Int deriving (Show, Eq)
-data Edge = Edge {s :: Vertex, e :: Vertex, w:: Int} deriving (Eq, Show)
+newtype Vertex = Vertex Int deriving (Show, Eq, Ord)
+data Edge = Edge {s :: Vertex, e :: Vertex, w:: Int} deriving (Eq, Show, Ord)
 
-type Graph = M.Map Int Edge
+type Graph = M.Map Vertex [Edge]
+type PHeap = H.PSQ Int Int
 
 mstCost :: 
     String ->
     IO Int
-mstCost = undefined
+mstCost path = do
+    g <- loadGraph path
+    let mst = heapMST g
+    let cost = foldr (\e x-> x + w e) 0 mst
+    pure cost
 
-loadGraph :: IO Graph
-loadGraph = undefined
+naiveMST :: Graph -> S.Set Edge
+naiveMST g =  undefined
+    --foreach node in V-X
+        --foreach edge not already in T
+            -- if edge has smallest cost & introduces a vertex not in V
+            -- then add e to T & v to X
+    -- stop when X == V
+
+heapMST :: Graph -> S.Set Edge
+heapMST g = step S.empty (S.singleton (Vertex 1)) initialHeap
+    where
+    initialHeap = H.fromList . fmap (\e -> e H.:-> w e) $ g M.! Vertex 1
+    step t x heap
+        | H.null heap = t
+        | otherwise = let
+            Just (edge H.:-> weight, heap') = H.minView heap
+            in if 
+                | S.member (s edge) x && S.member (e edge) x -> step t x heap'
+                | S.member (s edge) x -> let
+                    heapNewEdges = foldr (\e h'-> H.insert e (w e) h') heap' $ g M.! e edge
+                    in step (S.insert edge t) (S.insert (e edge) x) heapNewEdges
+                | S.member (e edge) x-> let
+                    heapNewEdges = foldr (\e h'-> H.insert e (w e) h') heap' $ g M.! s edge
+                    in step (S.insert edge t) (S.insert (s edge) x) heapNewEdges
+                | otherwise -> error "how did we get here"
+
+-- Load each edge in order, adding the edge to the given edge list for a start vertex & end vertex
+loadGraph ::
+    FilePath ->
+    IO Graph
+loadGraph path = foldl addEdge M.empty. lines <$> readFile path
+    where
+    addEdge :: Graph -> String -> Graph
+    addEdge g raw = 
+        case parse parseEdge "edge parser" raw of
+            Left e -> error (show e)
+            Right edge -> let 
+                g' = M.insertWith (++) (e edge) [edge] g
+                in M.insertWith (++) (s edge) [edge] g'
+
+parseEdge :: Parser Edge
+parseEdge = do
+    s <- Vertex <$> n
+    char ' '
+    e <- Vertex <$> n
+    char ' '
+    w <- n
+    pure Edge {s = s, e = e, w = w}
+    where n = read <$> many1 (char '-' <|> digit)
