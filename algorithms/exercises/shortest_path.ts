a773c532daf9f6c9601111f98c9f27bef6d8ec52
Chris Coffey <cmcof273@gmail.com>
2022-08-26 16:53:25 -0400

    Djikstras and floyd warshall

--- algorithms/exercises/shortest_path.ts
+++ algorithms/exercises/shortest_path.ts
@@ -3,15 +3,12 @@ import * as heap from './heap'
 
 type Vertex = number
 type WeightedEdge = {start: Vertex, end: Vertex, weight: number}
-interface Edges {
-  [index: Vertex]: WeightedEdge[]
-}
 
 type Graph = {
   vertices: Vertex[],
   edges: WeightedEdge[],
-  outboundEdges: Edges,
-  inboundEdges: Edges
+  outboundEdges: WeightedEdge[][],
+  inboundEdges: WeightedEdge[][]
 }
 
 
@@ -59,17 +56,100 @@ function bellmanFord(source: Vertex, graph: Graph): SingleSourceShortestPaths | 
   }
 }
 
+
 function djikstras(source: Vertex, graph: Graph): SingleSourceShortestPaths | undefined {
   let negativeEdge = false
   graph.edges.forEach((edge) => { if(edge.weight < 0) {negativeEdge = true} })
   if(negativeEdge) { return undefined }
 
+  const distances: number[] = []
+  distances[source] = 0
 
+  //compute distnace based on the shortest path from `source`
+  const dist = (e: WeightedEdge) => { return e.weight + distances[e.start]}
+  const compareEdges = (l: WeightedEdge, r: WeightedEdge) => {
+    if(dist(l) < dist(r)) { return heap.CompareResult.Lt }
+    if(dist(l) > dist(r)) { return heap.CompareResult.Gt }
+    else return heap.CompareResult.Eq
+  }
+
+  const frontier = new heap.MinHeap(compareEdges)
+  graph.outboundEdges[source].forEach((e) => frontier.insert(e) )
+
+  while(frontier.size() > 0) {
+    // Find the min crossing edge
+    const edge = frontier.extractMin()
+    if(edge === undefined) { console.log("Heap is empty!"); return undefined }
+
+    // Discard edges to vertices that already have shortest paths. This should be
+    // faster than pruning the heap
+    if(distances[edge.end] && distances[edge.end] < dist(edge)) { continue }
+
+    // Update the frontier
+    graph.outboundEdges[edge.end]
+      .filter((e) => {return distances[e.end] === undefined} )
+      .forEach((e) => frontier.insert(e))
+
+    // Update the known edges
+    distances[edge.end] = distances[edge.start] + edge.weight
+  }
 
   return {
     source: source,
-    paths: []
+    paths: distances
   }
+}
+
+function johnsons(graph: Graph): SingleSourceShortestPaths[] {
+  // Add an artificial vertex to each graph with a 0-weight edge to all other vertices
+  // Run Bellman-Ford to calculate shortest paths from artificial node to all vertices
+  // These shortest paths become vertex weights
+  // Adjust all edges by their vertex weights to make them positive
+  // Run Djikstras from each vertex
+  // Balance out the edge adjustments to restore the original path weights
+
+  return []
+}
+
+function floydWarshall(graph: Graph): SingleSourceShortestPaths[] | undefined {
+  // Convert to adjacency matrix representation to save space & speed up code
+  let adjacencyMatrix : number[][] = []
+  for(let n = 0; n < graph.vertices.length; n++) {
+    adjacencyMatrix[n] = new Array(graph.vertices.length).fill(Infinity)
+  }
+  graph.edges.forEach((e) => {
+    adjacencyMatrix[e.start][e.end] = e.weight
+  })
+  graph.vertices.forEach((v) => {
+    adjacencyMatrix[v][v] = 0
+  })
+
+  // Considers paths through an intermediate vertex, which must by definition be shortest
+  for(let k = 0; k < graph.vertices.length; k++) {
+    for(let i= 0; i < graph.vertices.length; i++) {
+      for(let j = 0; j < graph.vertices.length; j++) {
+        const distanceThroughK = adjacencyMatrix[i][k] + adjacencyMatrix[k][j]
+        if(distanceThroughK < adjacencyMatrix[i][j]) {
+          adjacencyMatrix[i][j] = distanceThroughK
+        }
+      }
+    }
+  }
+
+  // convert back from adjacency matrix
+  const result = adjacencyMatrix.map((weights, source) => {
+    return {
+      source: source,
+      paths: weights
+    }
+  })
+
+  // Detect negative cycle
+  for(let n=0; n < graph.vertices.length; n++) {
+    if(adjacencyMatrix[n][n] < 0 ) { return undefined }
+  }
+
+  return result
 }
 
 
@@ -82,8 +162,8 @@ function loadGraph(path: string): Graph {
   })
   edges.pop()
 
-  const inbound: Edges = {}
-  const outbound: Edges = {}
+  const inbound: WeightedEdge[][] = []
+  const outbound: WeightedEdge[][] = []
   const vertices: Vertex[] = []
   edges.forEach((edge) => {
     const inb = (inbound[edge.end] || [])
@@ -98,7 +178,7 @@ function loadGraph(path: string): Graph {
   })
 
   return {
-    vertices: [... new Set<Vertex>(vertices)],
+    vertices: [...new Set<Vertex>(vertices)],
     inboundEdges: inbound,
     outboundEdges: outbound,
     edges: edges
@@ -108,26 +188,12 @@ function loadGraph(path: string): Graph {
 
 
 const testGraph = loadGraph("data/g_test.txt")
-console.log(testGraph)
 
-
-const positiveTestGraph = {
-  vertices: [0,1,2,3,4],
-  edges: [
-    {start: 0, end: 1, weight: 6},
-    {start: 0, end: 4, weight: 7},
-    {start: 1, end: 4, weight: 8},
-    {start: 1, end: 2, weight: 5},
-    {start: 1, end: 3, weight: 4},
-    {start: 2, end: 1, weight: 2},
-    {start: 3, end: 2, weight: 7},
-    {start: 3, end: 0, weight: 2},
-    {start: 4, end: 2, weight: 3},
-    {start: 4, end: 3, weight: 9},
-  ],
-  outboundEdges: {},
-  inboundEdges: {},
-}
+const positiveTestGraph = loadGraph("data/g_test_pos.txt")
+console.log(positiveTestGraph)
 
 console.log(bellmanFord(0, testGraph))
 console.log(bellmanFord(0, positiveTestGraph))
+console.log(djikstras(0, positiveTestGraph))
+
+console.log(floydWarshall(testGraph))
